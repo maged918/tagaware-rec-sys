d6cbb3a3a0a1ca6384effb77b371e3322180f46a
hossam <hossam693@gmail.com>
2015-08-06 20:27:15 +0200

    created users dict, tags dict and problems dict

--- main.py
+++ main.py
@@ -15,17 +15,46 @@ To compute cosine similarity, dot product = size of intersection, magnitude = sq
 Parse Submissions.txt, generate dict of user_handle --> user_id (0-based)
 Length of dict is the number of users.
 '''
+f = open('Submissions.txt', 'r')
+handles = [line.split(' ')[1].rstrip()
+           for line in f if line.split(' ')[0] == 'Handle:']
+users = dict(zip(range(len(handles)), handles))
+print(users)
+f.close()
 
 '''
 Parse Problems-tags.txt, the unique problem name is ContestID + Index
 generate dict of problem_name --> problem_id (0-based)
 Length of dict is the number of problems
 '''
+f = open('Problems-tags.txt', 'r')
+contest = []
+index = []
+for line in f:
+  s = line.split(' ')
+  if s[0] == 'Contest':
+    contest += [int(s[2])]
+  if s[0] == 'Index:':
+    index += [s[1].rstrip()]
+problems = [str(c) + i for c, i in zip(contest, index)]
+problems = dict(zip(range(len(problems)), problems))
+print(problems)
+f.close()
 
 '''
 Parse Problems-tags.txt, generate dict of tag --> tag_id (0-based)
 Length of dict is the number of tags
 '''
+f = open('Problems-tags.txt', 'r')
+tags = set()
+for line in f:
+  s = line.split(' ')
+  if s[0] == 'Tags:':
+    tags |= set(line.split(' ', 1)[1:][0].rstrip().split(','))
+tags = dict(zip(range(len(tags)), map(lambda x: x.lstrip(), list(tags))))
+
+print(tags)
+f.close()
 
 ''''
 Serialize all dicts, and then load them later to save computation time.

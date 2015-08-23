8aa640abcbeb083d9e3ebd3aa80c612421fd8577
Tarek Mehrez <tarekmmehrez@gmail.com>
2015-08-23 16:32:51 +0200

    added script to produce xml files

---
+++ srcml_and_clean.sh
@@ -0,0 +1,18 @@
+# author: tarek
+# desc: takes in data dir path, produces xml files by srcml and cleans unnecessary namespaces
+cd $1
+for contest in $(ls); do
+	echo "processing $contest"
+	cd $contest
+	for index in $(ls); do
+		cd $index
+		echo "in $index"
+		for file in $(ls); do srcml $file -o $file.xml --in-order; done
+		sed -i -e 's/xmlns=\"http:\/\/www.srcML.org\/srcML\/src\"//g' *.xml 
+		sed -i -e 's/xmlns:cpp=\"http:\/\/www.srcML.org\/srcML\/cpp\"//g' *.xml
+		sed -i -e 's/cpp://g' *.xml		
+		rm *.xml-e
+		cd ..
+	done
+	cd ..
+done

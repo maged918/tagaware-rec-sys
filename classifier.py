f28e3c68518e971828907fb6c6ad939c36cc7f3a
Tarek Mehrez <tarekmmehrez@gmail.com>
2015-08-27 13:57:01 +0200

    added tag classification skeleton

--- classifier.py
+++ classifier.py
@@ -1,5 +1,7 @@
 import pickle
-
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
 
 f = open('features.pickle', 'rb')
 features = pickle.load(f)
@@ -17,3 +19,19 @@ with open("train-tags.txt") as f:
 			tags_list += arr[1].split(',')
 
 tags_list = list(set(tags_list))
+
+X_train=[]
+Y_train=[]
+
+for instance in features:
+	X_train.append(features[instance])
+	tags = []
+	for tag in labelled_tags[instance]:
+		tags.append(tags_list.index(tag))
+	Y_train.append(tags)
+
+Y_train = MultiLabelBinarizer().fit_transform(Y_train)
+
+clf = OneVsRestClassifier(SVC(kernel='poly'))
+clf.fit(X_train, Y_train)
+y = clf.predict(X_train)

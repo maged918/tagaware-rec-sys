4182596496228fed8ac84696019e67acf445e9de
Maged Shalaby <maged918@gmail.com>
2016-10-23 15:08:07 +0200

    generate single tag with priority rules (manual selection)

--- update_tags.py
+++ update_tags.py
@@ -9,8 +9,14 @@ desc: parses Problems-tags.txt to produce a file with the problem path alongside
 
 to_math = ['matrices', 'probabilities', 'combinatorics', 'number theory', 'chinese remainder theorem', 'games']
 to_graphs = ['dfs and similar', 'trees', 'shortest paths', 'graph matchings', 'flows']
-to_remove = ['bitmasks', 'dsu', 'hashing', 'ternary search','meet-in-the-middle', 'divide and conquer', '2-sat','schedules','fft']
+to_remove = ['bitmasks', 'dsu', 'hashing', 'ternary search','meet-in-the-middle', 'divide and conquer', '2-sat','schedules','fft', 'two pointers', 'constructive algorithms', 'binary search']
 to_strings = ['expression parsing', 'string suffix structures']
+
+single_sorting = ['implementation', 'sortings', 'brute force', 'greedy', 'math', 'data structures', 'dp', 'graphs', 'geometry', 'strings']
+single_dict = {}
+for i in range(len(single_sorting)):
+	single_dict[single_sorting[i]] = i
+
 # feed tag rules in here
 def update_tag(tag):
 	if tag in to_math:
@@ -23,7 +29,17 @@ def update_tag(tag):
 		tag = ''
 	return tag
 
+def single_tag(tag_list):
+	tag_so_far = ''
+	idx = -1
+	for tag in tag_list:
+		if single_dict[tag] > idx:
+			idx = single_dict[tag]
+			tag_so_far = tag
+	return tag_so_far
+
 out_file = open("data-set.txt",'w')
+single_file = open('data-set-single.txt', 'w')
 
 count_tags = defaultdict()
 count_tags = defaultdict(lambda: 0, count_tags)
@@ -33,6 +49,8 @@ count_pairs = defaultdict(lambda: 0, count_pairs)
 
 single_tags = defaultdict()
 single_tags = defaultdict(lambda: 0, single_tags)
+
+
 
 with open("Problems-tags.txt") as f:
 
@@ -48,10 +66,11 @@ with open("Problems-tags.txt") as f:
 
 			if arr_line[0].lower() == "tags:":
 				out_file.write(path +":")
+				single_file.write(path +":")
 				string = ''
 				tags = line.split(':')[1].strip()
 				tags_list = tags.split(',')
-				tags_list = [update_tag(tag.strip()) for tag in tags_list]
+				tags_list = [update_tag(tag.strip()) for tag in tags_list if update_tag(tag.strip())!='']
 				for i in tags_list:
 					string += i + ","
 					count_tags[i]+=1
@@ -62,6 +81,8 @@ with open("Problems-tags.txt") as f:
 					single_tags[tags_list[0]]+=1
 				# if '' not in tags_list:
 				out_file.write(string[:-1] + "\n")
+				single = single_tag(tags_list)
+				single_file.write(single + "\n")
 		else:
 			path = ''
 
@@ -74,3 +95,5 @@ for pair in sorted(count_pairs.items(), key=lambda x:x[1], reverse=True):
 
 for tag in sorted(single_tags.items(), key = lambda x:x[1], reverse=True):
 	print(tag[0], tag[1])
+
+print(len(single_tags))

b2666136ac32678b9f8f40d1b7a0428a58c5ce74
Maged Shalaby <maged918@gmail.com>
2016-11-18 13:21:24 +0200

    div 1 only

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 def get_div():
-    # div = ['Div1']
+    div = ['Div1']
     # div = ['Div2']
     # div = ['DivAll']
-    div = ['Div1', 'Div2', 'DivAll']
+    # div = ['Div1', 'Div2', 'DivAll']
     return div
 
 def get_ds_dir():
